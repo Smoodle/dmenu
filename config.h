d686e1983b10047f1f4cec03c279037a4953be6c
Smoodle <migwwe@gmail.com>
2020-05-08 00:36:01 +0100

    Changed font and colors

--- config.h
+++ config.h
@@ -6,17 +6,18 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Inconsolata Nerd Font:size=14"
+	"Hack Nerd Font:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d8dee9", "#2e3440" },
-	[SchemeSel] = { "#d8dee9", "#434c5e" },
-	[SchemeOut] = { "#d8dee9", "#2e3440" },
+	[SchemeNorm] = { "#bbbbbb", "#1d1f21" },
+	[SchemeSel] = { "#ffffff", "#cc6666" },
+	[SchemeOut] = { "#bbbbbb", "#373b41" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 20;
+
 
 /*
  * Characters not considered part of a word while deleting words
